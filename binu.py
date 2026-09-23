cc0adaf01ef817c80a88b86d975b30ff63d59152
Binu <binudio@outlook.de>
2026-09-23 14:57:44 +0200

    new file

---
+++ binu.py
@@ -0,0 +1,9 @@
+number = "(+123) 456-7890"
+
+print (number[0:6])
+print (number[11:])
+print (number[-1:])
+
+print(number[2:5])
+print(number[-4:])
+print(f"{number[2:5]}{number[7:-5]}{number[-4:]}")
